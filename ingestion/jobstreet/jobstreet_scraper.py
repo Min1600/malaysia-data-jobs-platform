ae72392d86c1f913237873b16e37648839858f08
Min1600 <aminhassan00@gmail.com>
2026-07-04 01:13:57 +0800

    completed most of jobstreet scraper

--- ingestion/jobstreet/jobstreet_scraper.py
+++ ingestion/jobstreet/jobstreet_scraper.py
@@ -1,5 +1,6 @@
 import json
 import random
+import math
 import re
 import time
 from datetime import datetime
@@ -11,37 +12,47 @@ HEADERS = {
     "Accept-Language": "en-US,en;q=0.9",
 }
 
-abs_path = "/home/aminh/workspace/web_scraper/data/raw/jobstreet"
-base_url = "https://my.jobstreet.com/data-analyst-jobs/in-Kuala-Lumpur"
-page_counter = 1
-total_collected = 0
-max_pages = 5 
+daily = 1
+weekly = 7
+biweekly = 14
+monthly = 31
 
-while page_counter <= max_pages:
-    print(f"🔄 Requesting Page {page_counter}...")
+BASE_URL = f"https://my.jobstreet.com/data-analyst-jobs/in-Kuala-Lumpur"
+ABS_PATH = "/home/aminh/workspace/web_scraper/data/raw/jobstreet"
 
-    # request specific page from jobstreet website
-    params = {"page": page_counter}
-    response = requests.get(base_url, params=params, headers=HEADERS, impersonate="chrome")
-    
-    # if no response end loop
+def get_total_pages(job_type, location = "Kuala Lumpur",frequency = None):
+    params = {
+    "keyword" : job_type,
+    "where" : location,
+    "daterange": frequency,
+    "page": 1
+    }
+    response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome")
+
     if response.status_code != 200:
-        print(f"🛑 Received a non-200 status code: {response.status_code}. Stopping pipeline.")
-        break
+        print(f"🛑 Received a non-200 status code: {response.status_code}. Stopping task.")
 
+    # use beautiful soup to get text from page
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # get no of jobs available on that day
+    no_of_jobs = soup.find(attrs={"data-automation": "totalJobsMessage"})
+    total_jobs = no_of_jobs.text if no_of_jobs else None
+
+    # conver to int type for calculation
+    total_jobs = int(re.sub(r'[^\d]', '', total_jobs))
+    # calculate number of pages (jobstreet has 30 jobs per page)
+    pages = math.ceil(int(total_jobs) / 30)
+
+    return pages
+
+def get_jobs(response, abs_path, total_collected, page_counter, frequency, seen_ids):
+    copies = 0
     # use beautiful soup to get text from page
     soup = BeautifulSoup(response.text, "html.parser")
     
     # locate all HTML containers that have content related to jobs
     job_cards = soup.find_all("article", attrs={"data-automation": "normalJob"})
-    
-    # error handling if no job cards found or wrong attribute name
-    if not job_cards:
-        print("🛑 Could not find job listings on the page. Format might have changed or end of pagination.")
-        break
-        
-    print(f"   📊 Found {len(job_cards)} job listings on this page.")
-    total_collected += len(job_cards)
 
     # iterate over each HTML container of elements to get data from one job at a time
     for card in job_cards:
@@ -58,6 +69,13 @@ while page_counter <= max_pages:
         # Parse the unique job ID from the URL (usually a sequence of numbers at the end)
         job_id_match = re.search(r'/job/(\d+)', job_url)
         job_id = job_id_match.group(1) if job_id_match else job_url.split("/")[-1]
+
+        if job_id in seen_ids:
+            copies += 1
+            total_collected -= 1
+            continue
+        
+        seen_ids.add(job_id)
 
         # Extract meta elements from card layout
         company_el = card.find("a", attrs={"data-automation": "jobCompany"})
@@ -78,7 +96,10 @@ while page_counter <= max_pages:
             full_desc = desc_el.text.strip() if desc_el else ""
         else:
             print(f"❌ Skipped ID {job_id}: Detail page unreachable.")
+            total_collected -= 1
             continue
+
+        total_collected += 1
 
         # format data to json
         raw_record = {
@@ -86,7 +107,7 @@ while page_counter <= max_pages:
             "source": "JobStreet",
             "url": job_url,
             "collection_timestamp": datetime.utcnow().isoformat(),
-            "job_title": title_el.text.strip() if title_el else "N/A,
+            "job_title": title_el.text.strip() if title_el else "N/A",
             "company": company_el.text.strip() if company_el else "N/A",
             "location": location_el.text.strip() if location_el else "N/A",
             "employment_type": "N/A", # Will pull from description processing step
@@ -100,17 +121,58 @@ while page_counter <= max_pages:
             "raw_html": str(desc_el) if desc_el else ""
         }
 
+        if frequency == 1:
+            filename = f"{abs_path}/{datetime.now().strftime('%d-%m-%Y')}.jsonl"
+        else:
+            filename = f"{abs_path}/historic.jsonl"
+
         # save to jsonl file
-        filename = f"{abs_path}/historic.jsonl"
         with open(filename, "a", encoding="utf-8") as f:
             json_line = json.dumps(raw_record, ensure_ascii=False)
             f.write(json_line + "\n")
 
-        print(f"✅ Fully ingested: {raw_record['job_title']} at {raw_record['company']} -> Saved to {filename}")
+    print(copies)
+    print(f"Collected {total_collected} new jobs on Page {page_counter}")
+    return total_collected
+
+def _run_scrape(job_type, location = "Kuala Lumpur", frequency = None):
+
+    page_counter = 1
+    total_collected = 0
+    seen_ids = set()
+    max_pages = get_total_pages(job_type, location ,frequency)
+
+    while page_counter <= max_pages:
+        print(f"🔄 Requesting Page {page_counter}...")
+
+        # request specific page from jobstreet website
+        params = {
+            "keyword" : job_type,
+            "where" : location,
+            "daterange": frequency,
+            "page": page_counter
+        }
+
+        response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome")
         
-        time.sleep(random.uniform(3.0, 6.0))
+        # if no response end loop
+        if response.status_code != 200:
+            print(f"🛑 Received a non-200 status code: {response.status_code}. Stopping pipeline.")
+            break
+            
+        total_collected += get_jobs(response, ABS_PATH, total_collected, page_counter, frequency, seen_ids)
+        print(total_collected)
+        page_counter += 1
+        time.sleep(random.uniform(4.0, 8.0))
 
-    page_counter += 1
-    time.sleep(random.uniform(4.0, 8.0))
+    return total_collected
 
-print("Job Complete!")
+def js_scraper(job_type, location):
+    total_collected = _run_scrape(job_type, location)
+    print(f"\n✅ Full run complete. Successfully captured {total_collected} jobstreet {job_type} listings!")
+
+def js_daily_scraper(job_type, location):
+    total_collected = _run_scrape(job_type, location, daily)
+    print(f"\n✨ Daily run complete. Successfully captured {total_collected} jobstreet {job_type} listings!")
+
+#js_daily_scraper('Data Analyst', 'Kuala Lunpur')
