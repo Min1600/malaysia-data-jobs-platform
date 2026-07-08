7119fa6f3b4b467349059ca675d0ff4db9e2cc36
Min1600 <aminhassan00@gmail.com>
2026-07-08 12:29:53 +0800

    added logging to jobstreet scraper

--- ingestion/jobstreet/jobstreet_scraper.py
+++ ingestion/jobstreet/jobstreet_scraper.py
@@ -19,6 +19,8 @@ ABS_PATH = "/home/aminh/workspace/web_scraper/data/raw/jobstreet"
 # scraping timeline, if None then scrape all job listings
 daily, weekly, monthly = 1,7,31
 
+js_logger = logging.getLogger(__name__)
+
 def get_total_pages(job_type, location = "Kuala Lumpur",date_range = None):
     """
     calculates total number of pages of job listings for the specific job type, location and dates provided
@@ -39,18 +41,33 @@ def get_total_pages(job_type, location = "Kuala Lumpur",date_range = None):
     "page": 1
     }
 
-    # requests data from jobstreet
-    response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome")
+    # test connection 
+    try:
 
-    if response.status_code != 200:
-        print(f"🛑 Received a non-200 status code: {response.status_code}. Stopping task.")
+        # requests data from jobstreet
+        response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome", timeout = 10)
+        response.raise_for_status() # Automatically triggers HTTPError if status is 4xx or 5xx
+    
+    # Catches bad status codes (4xx or 5xx)
+    except requests.exceptions.HTTPError as e:
+        js_logger.error(f"🛑 HTTP Error occurred: {e.response.status_code} - {e.response.reason}. Unable to determine number of pages aborting task!")
+        return None
+    
+    # Catches connection drops, timeouts, DNS issues where NO response was given
+    except requests.exceptions.RequestException as e:
+        js_logger.error(f"💥 Network level error occurred (No response received): {e}. Unable to determine number of pages aborting task!")
+        return None
 
     # use beautiful soup to get text from page
     soup = BeautifulSoup(response.text, "html.parser")
     
     # get no of jobs available
     no_of_jobs = soup.find(attrs={"data-automation": "totalJobsMessage"})
-    total_jobs = no_of_jobs.text if no_of_jobs else None
+
+    if no_of_jobs:
+        total_jobs = no_of_jobs.text
+    else:
+        return None
 
     # convert to int type for calculation
     total_jobs = int(re.sub(r'[^\d]', '', total_jobs))
@@ -63,14 +80,13 @@ def get_total_pages(job_type, location = "Kuala Lumpur",date_range = None):
 
 
 
-def get_jobs(response, filename, total_collected, seen_ids):
+def get_jobs(response, filename, seen_ids):
     """
     Scrape job listings from jobstreet and save them to a jsonl file
 
     Args:
         response: requested data from jobstreet webpage
         filename: name and location of file to save the scraped data
-        total_collected: number of jobs collected so far
         seen_ids: set containing jobs ids of jobs already saved
 
     Returns:
@@ -82,6 +98,7 @@ def get_jobs(response, filename, total_collected, seen_ids):
     
     # locate all job listings on website page
     job_cards = soup.find_all("article", attrs={"data-automation": "normalJob"})
+    num_jobs = len(job_cards)
 
     # iterate over all job listings found to get data from each one
     for card in job_cards:
@@ -101,7 +118,7 @@ def get_jobs(response, filename, total_collected, seen_ids):
 
         # locates any repeated jobs
         if job_id in seen_ids:
-            total_collected -= 1
+            num_jobs -= 1
             continue
         
         # removes repeated jobs by adding to set() data type
@@ -129,10 +146,8 @@ def get_jobs(response, filename, total_collected, seen_ids):
         # if no response from description page skip job (dont write to file)
         else:
             print(f"❌ Skipped ID {job_id}: Detail page unreachable.")
-            total_collected -= 1
+            num_jobs -= 1
             continue
-
-        total_collected += 1
 
         # format data to json
         raw_record = {
@@ -154,12 +169,23 @@ def get_jobs(response, filename, total_collected, seen_ids):
             "raw_html": str(desc_el) if desc_el else ""
         }
 
-        # save to jsonl file
-        with open(filename, "a", encoding="utf-8") as f:
-            json_line = json.dumps(raw_record, ensure_ascii=False)
-            f.write(json_line + "\n")
+        try:
+            # save to jsonl file
+            with open(filename, "a", encoding="utf-8") as f:
+                json_line = json.dumps(raw_record, ensure_ascii=False)
+                f.write(json_line + "\n")
 
-    return total_collected
+        except IOError as e:
+            # Catches disk full, permission denied, or missing directory errors
+            js_logger.error(f"💾 File write failed! Could not append job to {filename}. Error: {e}")
+            return num_jobs
+        
+        except TypeError as e:
+            # Catches situations where raw_record contains an object json cannot serialize (e.g., datetime objects)
+            js_logger.error(f"JSON serialization failed for job data. Serialization Error: {e}")
+            return num_jobs
+
+    return num_jobs
 
 
 
@@ -182,9 +208,13 @@ def _run_scrape(job_type, location = "Kuala Lumpur", date_range = None):
     seen_ids = set()
     max_pages = get_total_pages(job_type, location ,date_range)
 
+    if not max_pages:
+        return total_collected
+
     # requests data as long as there are pages to scrape
     while page_counter <= max_pages:
-        print(f"🔄 Requesting Page {page_counter}...")
+
+        js_logger.info(f"🔄 Requesting Page {page_counter}...")
 
         # request specific page from jobstreet website
         params = {
@@ -194,14 +224,21 @@ def _run_scrape(job_type, location = "Kuala Lumpur", date_range = None):
             "page": page_counter
         }
 
-        # requests data from jobstreet
-        response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome")
+        # test connection 
+        try:
+            response = requests.get(BASE_URL, params=params, headers=HEADERS, impersonate="chrome", timeout = 10)
+            response.raise_for_status() # Automatically triggers HTTPError if status is 4xx or 5xx
         
-        # if no response end loop
-        if response.status_code != 200:
-            print(f"🛑 Received a non-200 status code: {response.status_code}. Stopping pipeline.")
+        # Catches bad status codes (4xx or 5xx)
+        except requests.exceptions.HTTPError as e:
+            js_logger.error(f"🛑 HTTP Error occurred: {e.response.status_code} - {e.response.reason}. Stopping task.")
             break
         
+        # Catches connection drops, timeouts, DNS issues where NO response was given
+        except requests.exceptions.RequestException as e:
+            js_logger.error(f"💥 Network level error occurred (No response received): {e}. Stopping task.")
+            break
+
         # Save jobs with a timeline scraped on the same day into its own file
         if date_range is None:
             filename = f"{abs_path}/historic.jsonl"
@@ -209,9 +246,12 @@ def _run_scrape(job_type, location = "Kuala Lumpur", date_range = None):
             filename = f"{abs_path}/{datetime.now().strftime('%d-%m-%Y')}.jsonl"
         
         # get total number of jobs on current page
-        total_collected += get_jobs(response, filename, total_collected, seen_ids)
+        num_jobs = get_jobs(response, filename, seen_ids)
 
-        print(f"Collected {total_collected} new jobs on Page {page_counter}")
+        # add to final total
+        total_collected += num_jobs
+
+        js_logger.info(f"Collected {num_jobs} new jobs on Page {page_counter}")
 
         page_counter += 1
         time.sleep(random.uniform(4.0, 8.0)) # Anti-bot protection
@@ -239,9 +279,10 @@ def js_scraper(job_type, location = "Kuala Lumpur", date_range = None):
     # no date_range given means scrape all available data on jobstreet webapge
     if date_range is None:
 
+        js_logger.info(f"Collecting all job street {job_type} job listings.")
         # run the jobstreet job scraper and get total number of jobs collected
         total_collected = _run_scrape(job_type, location, date_range)
-        print(f"\n✅ Full run complete. Successfully saved {total_collected} {job_type} jobs from jobstreet listings!")
+        js_logger.info(f"\n✅ Full run complete. Successfully saved {total_collected} {job_type} jobs from jobstreet listings!")
 
     # scrape based on date_range timeline given
     elif date_range in [daily, weekly, monthly]:
@@ -253,15 +294,16 @@ def js_scraper(job_type, location = "Kuala Lumpur", date_range = None):
             monthly: "last month"
         }
 
-        # run the jobstreet job scraper and get total number of jobs collected
-        total_collected = _run_scrape(job_type, location, date_range)
 
         # get the phrase to use based on the date_range given
         freq_type = timelines[date_range]
 
-        print(f"✨ Full run complete. Successfully saved {total_collected} {job_type} job listings from jobstreet, posted within the {freq_type}")
-    else:
-        print('Error, input needs to be 1,7,31 or None')
-    
+        js_logger.info(f"Collecting all jobstreet {job_type} job listings from the {freq_type}")
 
-#js_daily_scraper('Data Analyst', 'Kuala Lunpur')
+        # run the jobstreet job scraper and get total number of jobs collected
+        total_collected = _run_scrape(job_type, location, date_range)
+
+        js_logger.info(f"✨ Full run complete. Successfully saved {total_collected} {job_type} job listings from jobstreet, posted within the {freq_type}")
+    else:
+        js_logger.info('Error, input needs to be 1,7,31 or None')
+    
