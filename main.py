dfd7c134439e10cc3d0b8185ff22b6163bdf4e91
Min1600 <aminhassan00@gmail.com>
2026-07-17 17:37:05 +0800

    edit data to show

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def fetch_data(website):
         df = pd.read_json(local_file_path, lines=True)
         main_logger.info(f"📊 Successfully loaded {len(df)} jobs rows from {website} uploaded on {current_time}.")
 
-        target_columns = ["job_title", "company", "salary_min", "posting_date", "industry", "skills", "employment_type"]
+        target_columns = ["job_title", "company", "url", "posting_date", "industry", "skills"]
         existing_columns = [col for col in target_columns if col in df.columns]
         
         return df[existing_columns]
